da63b9c0328d263d5ad652c62a5a848c07a80b55
Dorcas29 <dorcasmutyavaviri@gmail.com>
2024-07-14 14:14:46 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hymns = [
-        { title: "Amazing Grace", lyrics: "Amazing grace! How sweet the sound..." },
+        { title: "Ishe Taungana", lyrics: "Amazing grace! How sweet the sound..." },
         { title: "Great is Thy Faithfulness", lyrics: "Great is Thy faithfulness, O God my Father..." },
         { title: "How Great Thou Art", lyrics: "O Lord my God, when I in awesome wonder..." },
         // Add more hymns as needed
